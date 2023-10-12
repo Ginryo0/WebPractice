94767545049e5a14b3631ae64c8c850e82ec12ed
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-10-12 14:30:25 +0300

    use get static props

---
+++ 02-NextJs  -old- Max/01 - Pre rendering/pages/index.js
@@ -0,0 +1,24 @@
+function HomePage(props) {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((prod) => (
+        <li key={prod.id}>{prod.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { id: 'p1', title: 'Product 1' },
+        { id: 'p2', title: 'Product 2' },
+        { id: 'p3', title: 'Product 3' },
+      ],
+    },
+  };
+}
+
+export default HomePage;
