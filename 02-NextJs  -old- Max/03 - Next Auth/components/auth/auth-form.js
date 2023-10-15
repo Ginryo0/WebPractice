83475c4411d38aa64868d9082e565c31182c7888
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-10-15 06:11:08 +0300

    add sign up

--- 02-NextJs  -old- Max/03 - Next Auth/components/auth/auth-form.js
+++ 02-NextJs  -old- Max/03 - Next Auth/components/auth/auth-form.js
@@ -1,29 +1,67 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import classes from './auth-form.module.css';
+
+async function createUser(email, password) {
+  const res = await fetch('api/auth/signup', {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || 'smth went wrong');
+  }
+
+  return data;
+}
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const emailRef = useRef();
+  const pwRef = useRef();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const enteredPw = pwRef.current.value;
+
+    if (isLogin) {
+      // log user in
+    } else {
+      try {
+        const res = await createUser(enteredEmail, enteredPw);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={pwRef} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
