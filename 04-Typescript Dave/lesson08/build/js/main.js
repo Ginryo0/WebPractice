f3141deaf2d5bf4f22ae9352073b797290cfec9b
Gin Ryo <ginryoix@gmail.com>
2024-02-27 21:46:55 +0200

    add tsc lesson 8

---
+++ 04-Typescript Dave/lesson08/build/js/main.js
@@ -0,0 +1,136 @@
+"use strict";
+// Generic type
+// <T> -> define T type variable placeholder to use it in function definition
+const echo = (arg) => arg;
+//////////////////////////////////
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+};
+console.log(isObj(true));
+console.log(isObj('John'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'John' }));
+console.log(isObj(null));
+///////////////////////////////////
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    // double ! -> to return a boolean of any value
+    return { arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({})); // modified
+console.log(isTrue({ name: 'Dave' }));
+console.log(isTrue([])); // modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+const processUser = (user) => {
+    // process the user with logic here 
+    return user;
+};
+console.log(processUser({ id: 1, name: 'Dave' }));
+//console.log(processUser({ name: 'Dave'}))
+///////////////////////////////////////
+// Types:
+// T extends HasID -> object that is guaranteed to have id prop
+// K extends keyof T -> one of the keys of the objects passed -> 'id' | 'username' | 'email' ...tc
+// Params -> T[], K
+// Returns -> array of T[K] (object value types)
+const getUsersProperty = (users, key) => {
+    return users.map(user => user[key]);
+};
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        },
+        "bonus": "bonus"
+    },
+];
+console.log(getUsersProperty(usersArray, "email"));
+console.log(getUsersProperty(usersArray, "username"));
+console.log(getUsersProperty(usersArray, "id"));
+// I have added bonus to one object, It was able to infer it as optional
+console.log(getUsersProperty(usersArray, "bonus"));
+///////////////////////////////////////
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const store = new StateObject("John");
+console.log(store.state);
+store.state = "Dave";
+//store.state = 12
+// you can manually specify the generic type so that it accepts multiple types
+const myState = new StateObject([15]);
+myState.state = ['Dave', 42, true];
+console.log(myState.state);
