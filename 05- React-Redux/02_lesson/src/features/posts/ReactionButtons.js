cb062a353dc4abfc8982c8d92757e6cb58ff7c7e
Ahmed Zeid <ginryoix@gmail.com>
2024-12-12 09:24:19 +0200

    Lesson 2

---
+++ 05- React-Redux/02_lesson/src/features/posts/ReactionButtons.js
@@ -0,0 +1,34 @@
+import { useDispatch } from 'react-redux';
+import { reactionAdded } from './postsSlice';
+
+const reactionEmoji = {
+  thumbsUp: '👍',
+  wow: '😮',
+  heart: '❤️',
+  rocket: '🚀',
+  coffee: '☕',
+};
+
+const ReactionButtons = ({ post }) => {
+  const dispatch = useDispatch();
+
+  // Object Entries => array of entries [Key, Value]
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        className="reactionButton"
+        onClick={() =>
+          // dispatch(action(payload object))
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji} {post.reactions[name]}
+      </button>
+    );
+  });
+
+  return <div>{reactionButtons}</div>;
+};
+export default ReactionButtons;
